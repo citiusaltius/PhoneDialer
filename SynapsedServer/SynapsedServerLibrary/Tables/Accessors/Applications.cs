9a5949b5b39540897a043c7272ea255facfdc430
citiusaltius <ian+development@aiwong.com>
2016-04-05 22:25:45 -0400

    Unfinished Applications, ApplicationMemberships

---
+++ SynapsedServer/SynapsedServerLibrary/Tables/Accessors/Applications.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using Amazon.DynamoDBv2.Model;
+using Amazon.DynamoDBv2;
+using SynapsedServerLibrary.Tables.Defines;
+using SynapsedServerLibrary.Utilities;
+
+namespace SynapsedServerLibrary.Tables.Accessors
+{
+    public class Applications : TableObject
+    {
+        new DefinitionsApplications ThisTableDefinition = new DefinitionsApplications();
+
+        public Applications()
+        {
+            ThisTableDefinition = new DefinitionsApplications();
+        }
+
+
+        public Model.Community Create(Model.Community ObjectIn)
+        {
+            throw new System.NotImplementedException();
+            Debug.WriteLine("===Communities.Create===");
+            // Request new CommunityId
+            //TableCounters TableCounterClient = new TableCounters();
+            //int NewCommunityId = TableCounterClient.GetAndIncrement(ThisTableDefinition.TableName);
+            //ObjectIn.CommunityId = (NewCommunityId);
+
+            return (Model.Community)base.Create(ObjectIn, ThisTableDefinition.TableName);
+        }
+
+        public Model.Community Get(int CommunityId)
+        {
+            Debug.WriteLine("===Communities.GetInformation===");
+            Debug.WriteLine("Retrieving for " + CommunityId);
+            Model.Community retval = new Model.Community();
+            retval.CommunityId = (CommunityId);
+            GetItemRequest req = new GetItemRequest();
+            req.Key.Add(ThisTableDefinition.Index, new AttributeValue() { N = CommunityId.ToString() });
+            req.TableName = ThisTableDefinition.TableName;
+
+            GetItemResponse resp;
+            try
+            {
+                resp = client.GetItem(req);
+            }
+            catch (AmazonDynamoDBException e)
+            {
+                Debug.WriteLine(e.Message);
+                Debug.WriteLine(e.StackTrace);
+                throw e;
+            }
+            //// Get EntityName
+            //retval.SetCommunityName(resp.Item[DefinitionsCommunities.FieldNameCommunityName].S);
+            //// Convert string to CommunityType
+            //retval.SetCommunityType(DefinitionsCommunities.GetCommunityTypeFromString(resp.Item[DefinitionsCommunities.FieldNameCommunityType].S));
+
+            //SetObjectBasePropertiesFromItem(retval, resp.Item);
+
+            retval.LoadFromItem(resp.Item);
+            return retval;
+        }
+
+        public List<Model.Community> GetAll()
+        {
+            Debug.WriteLine("===Communities.GetAll===");
+
+            ScanRequest req = new ScanRequest();
+            req.TableName = ThisTableDefinition.TableName;
+
+            ScanResponse resp;
+
+            try
+            {
+                resp = client.Scan(req);
+            }
+            catch (AmazonDynamoDBException e)
+            {
+                Debug.WriteLine(e.Message);
+                Debug.WriteLine(e.StackTrace);
+                throw e;
+            }
+
+            List<Model.Community> AllCommunities = new List<Model.Community>();
+            foreach (Dictionary<string, AttributeValue> Item in resp.Items)
+            {
+                Model.Community NewCommunity = new Model.Community();
+                NewCommunity.LoadFromItem(Item);
+                AllCommunities.Add(NewCommunity);
+            }
+            return AllCommunities;
+        }
+    }
+}
---
+++ SynapsedServer/SynapsedServerLibrary/Tables/Defines/DefinitionsApplicationMemberships.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynapsedServerLibrary.Tables.Defines
+{
+    public class DefinitionsApplicationMemberships : TableDefinitions
+    {
+        public new string TableName = "ApplicationMemberships";
+        public new string Index = "ApplicationMembershipId";
+        public const string FieldNameEntityId = "EntityId";
+        public const string FieldNameApplicationId = "ApplicationId";
+
+
+        public enum ApplicationMembershipTypes : int
+        {
+            Undefined,
+            Owner,
+            Administrator,
+            Member
+        };
+    }
+}
